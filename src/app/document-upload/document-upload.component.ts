7d115a28f885bdb5ca10b9c630cd58952932ecf8
Sandeep Alla <A1821331@IIRCGA255>
2018-07-18 16:49:20 -0500

    Form Validations

--- src/app/document-upload/document-upload.component.ts
+++ src/app/document-upload/document-upload.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {UsersService} from '../shared/users.service';
 import {Users} from '../users';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-document-upload',
@@ -13,7 +14,9 @@ export class DocumentUploadComponent implements OnInit {
 
   selectedFiles: FileList;
   private users:Users;
-  constructor(private usersService:UsersService, private router:Router) { }
+  uploadForm: FormGroup;
+  submitted = false;
+  constructor(private usersService:UsersService, private router:Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {   
    
@@ -24,11 +27,24 @@ export class DocumentUploadComponent implements OnInit {
           'routingNum' : null,
           'accountNum': null
         }
+
+        this.uploadForm = this.formBuilder.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+          routingNum: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+          accountNum: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(17)]]
+      });
       
   } 
 
+  get f() { return this.uploadForm.controls; }
+
   documentUpdate(){
-    
+      this.submitted = true;
+      if (this.uploadForm.invalid) {
+        return;
+    }
       this.usersService.createUsers(this.users).subscribe(
         data=>{
           console.log(data);          
