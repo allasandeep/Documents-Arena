93f8434a3c648e7eb922de449feb565a0e195656
Sandeep Alla <A1821331@IIRCGA255>
2018-07-18 16:50:22 -0500

    Erro Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HomeComponent } from './home/home.component';
 import { DocumentUploadComponent } from './document-upload/document-upload.component';
 import { DocumentDownloadComponent } from './document-download/document-download.component';
 import { ListComponent } from './list/list.component';
-import { CreateUpdateComponent } from './create-update/create-update.component';
 import { ROUTING } from './app.routing';
 import { UsersService } from './shared/users.service';
 import {HttpClientModule } from '@angular/common/http';
@@ -20,8 +19,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     HomeComponent,
     DocumentUploadComponent,
-    DocumentDownloadComponent,
-    CreateUpdateComponent,
+    DocumentDownloadComponent,    
     ListComponent
   ],
   imports: [
